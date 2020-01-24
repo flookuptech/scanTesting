ccf6d0bcf78097ac9dc23e4f9cc2bdbceb7006b6
Aditya Sanil <aditya.sanil@somaiya.edu>
2020-01-24 20:20:21 +0530

    Added decrypt code

--- App.js
+++ App.js
@@ -1,148 +1,170 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {
-    TouchableOpacity,
-    Text,
-    StatusBar,
-    Linking,
-    View,
-    StyleSheet
+  TouchableOpacity,
+  Text,
+  StatusBar,
+  Linking,
+  View,
+  StyleSheet,
 } from 'react-native';
 
+import CryptoJS from 'crypto-js';
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scan: false,
-            ScanResult: false,
-            result: null
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      scan: false,
+      ScanResult: false,
+      result: null,
+    };
+  }
+
+  onSuccess = e => {
+    const check = e.data.substring(0, 4);
+    console.log('scanned data' + check);
+    this.setState({
+      result: e,
+      scan: false,
+      ScanResult: true,
+    });
+    if (check === 'http') {
+      Linking.openURL(e.data).catch(err =>
+        console.error('An error occured', err),
+      );
+    } else {
+      this.setState({
+        result: e,
+        scan: false,
+        ScanResult: true,
+      });
     }
+  };
 
-    onSuccess = (e) => {
-        const check = e.data.substring(0, 4);
-        console.log('scanned data' + check);
-        this.setState({
-            result: e,
-            scan: false,
-            ScanResult: true
-        })
-        if (check === 'http') {
-            Linking
-                .openURL(e.data)
-                .catch(err => console.error('An error occured', err));
+  activeQR = () => {
+    this.setState({
+      scan: true,
+    });
+  };
+  scanAgain = () => {
+    this.setState({
+      scan: true,
+      ScanResult: false,
+    });
+  };
 
+  decrypt = data => {
+    const test = CryptoJS.AES.decrypt(data, 'k');
+    var originalText = bytes.toString(CryptoJS.enc.Utf8);
+    return originalText;
+  };
 
-        } else {
-            this.setState({
-                result: e,
-                scan: false,
-                ScanResult: true
-            })
-        }
+  render() {
+    const {scan, ScanResult, result} = this.state;
+    const desccription =
+      'QR code (abbreviated from Quick Response Code) is the trademark for a type of matrix barcode (or two-dimensional barcode) first designed in 1994 for the automotive industry in Japan. A barcode is a machine-readable optical label that contains information about the item to which it is attached. In practice, QR codes often contain data for a locator, identifier, or tracker that points to a website or application. A QR code uses four standardized encoding modes (numeric, alphanumeric, byte/binary, and kanji) to store data efficiently; extensions may also be used.';
+    return (
+      <View style={styles.scrollViewStyle}>
+        <Fragment>
+          <StatusBar barStyle="dark-content" />
+          <Text style={styles.textTitle}>
+            Welcome To React-Native QR Code Tutorial !
+          </Text>
+          {!scan && !ScanResult && (
+            <View style={styles.cardView}>
+              <Text numberOfLines={8} style={styles.descText}>
+                {desccription}
+              </Text>
 
-    }
-
-    activeQR = () => {
-        this.setState({
-            scan: true
-        })
-    }
-    scanAgain = () => {
-        this.setState({
-            scan: true,
-            ScanResult: false
-        })
-    }
-    render() {
-        const { scan, ScanResult, result } = this.state
-        const desccription = 'QR code (abbreviated from Quick Response Code) is the trademark for a type of matrix barcode (or two-dimensional barcode) first designed in 1994 for the automotive industry in Japan. A barcode is a machine-readable optical label that contains information about the item to which it is attached. In practice, QR codes often contain data for a locator, identifier, or tracker that points to a website or application. A QR code uses four standardized encoding modes (numeric, alphanumeric, byte/binary, and kanji) to store data efficiently; extensions may also be used.'
-        return (
-            <View style={styles.scrollViewStyle}>
-                <Fragment>
-                    <StatusBar barStyle="dark-content" />
-                    <Text style={styles.textTitle}>Welcome To React-Native QR Code Tutorial !</Text>
-                    {!scan && !ScanResult &&
-                        <View style={styles.cardView} >
-                            <Text numberOfLines={8} style={styles.descText}>{desccription}</Text>
-
-                            <TouchableOpacity onPress={this.activeQR} style={styles.buttonTouchable}>
-                                <Text style={styles.buttonTextStyle}>Click to Scan !</Text>
-                            </TouchableOpacity>
-
-                        </View>
-                    }
-
-                    {ScanResult &&
-                        <Fragment>
-                            <Text style={styles.textTitle1}>Result !</Text>
-                            <View style={ScanResult ? styles.scanCardView : styles.cardView}>
-                                <Text>Type : {result.type}</Text>
-                                <Text>Result : {result.data}</Text>
-                                <Text numberOfLines={1}>RawData: {result.rawData}</Text>
-                                <TouchableOpacity onPress={this.scanAgain} style={styles.buttonTouchable}>
-                                    <Text style={styles.buttonTextStyle}>Click to Scan again!</Text>
-                                </TouchableOpacity>
-
-                            </View>
-                        </Fragment>
-                    }
-
-
-                    {scan &&
-                        <QRCodeScanner
-                            reactivate={true}
-                            showMarker={true}
-                            ref={(node) => { this.scanner = node }}
-                            onRead={this.onSuccess}
-                            topContent={
-                                <Text style={styles.centerText}>
-                                    Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code to test.</Text>
-                            }
-                            bottomContent={
-                                <View>
-                                    <TouchableOpacity style={styles.buttonTouchable} onPress={() => this.scanner.reactivate()}>
-                                        <Text style={styles.buttonTextStyle}>OK. Got it!</Text>
-                                    </TouchableOpacity>
-
-                                    <TouchableOpacity style={styles.buttonTouchable} onPress={() => this.setState({ scan: false })}>
-                                        <Text style={styles.buttonTextStyle}>Stop Scan</Text>
-                                    </TouchableOpacity>
-                                </View>
-
-                            }
-                        />
-                    }
-                </Fragment>
+              <TouchableOpacity
+                onPress={this.activeQR}
+                style={styles.buttonTouchable}>
+                <Text style={styles.buttonTextStyle}>Click to Scan !</Text>
+              </TouchableOpacity>
             </View>
+          )}
 
-        );
-    }
+          {ScanResult && (
+            <Fragment>
+              <Text style={styles.textTitle1}>Result !</Text>
+              <View style={ScanResult ? styles.scanCardView : styles.cardView}>
+                <Text>Type : {result.type}</Text>
+                <Text>Result : {this.decrypt(result)}</Text>
+                <Text numberOfLines={1}>RawData: {result.rawData}</Text>
+                <TouchableOpacity
+                  onPress={this.scanAgain}
+                  style={styles.buttonTouchable}>
+                  <Text style={styles.buttonTextStyle}>
+                    Click to Scan again!
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </Fragment>
+          )}
+
+          {scan && (
+            <QRCodeScanner
+              reactivate={true}
+              showMarker={true}
+              ref={node => {
+                this.scanner = node;
+              }}
+              onRead={this.onSuccess}
+              topContent={
+                <Text style={styles.centerText}>
+                  Go to{' '}
+                  <Text style={styles.textBold}>
+                    wikipedia.org/wiki/QR_code
+                  </Text>{' '}
+                  on your computer and scan the QR code to test.
+                </Text>
+              }
+              bottomContent={
+                <View>
+                  <TouchableOpacity
+                    style={styles.buttonTouchable}
+                    onPress={() => this.scanner.reactivate()}>
+                    <Text style={styles.buttonTextStyle}>OK. Got it!</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={styles.buttonTouchable}
+                    onPress={() => this.setState({scan: false})}>
+                    <Text style={styles.buttonTextStyle}>Stop Scan</Text>
+                  </TouchableOpacity>
+                </View>
+              }
+            />
+          )}
+        </Fragment>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   scrollViewStyle: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#99003d'
-},
+    backgroundColor: '#99003d',
+  },
 
-textTitle: {
+  textTitle: {
     fontWeight: 'bold',
     fontSize: 18,
     textAlign: 'center',
     padding: 16,
-    color: 'white'
-},
-textTitle1: {
+    color: 'white',
+  },
+  textTitle1: {
     fontWeight: 'bold',
     fontSize: 18,
     textAlign: 'center',
     padding: 16,
-    color: 'black'
-},
-cardView: {
+    color: 'black',
+  },
+  cardView: {
     //width: deviceWidth - 32,
     //height: deviceHeight / 2,
     alignSelf: 'center',
@@ -153,16 +175,16 @@ cardView: {
     borderColor: '#ddd',
     borderBottomWidth: 0,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.8,
     shadowRadius: 2,
     elevation: 4,
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10,
-    backgroundColor: 'white'
-},
-scanCardView: {
+    backgroundColor: 'white',
+  },
+  scanCardView: {
     //width: deviceWidth - 32,
     //height: deviceHeight / 2,
     alignSelf: 'center',
@@ -173,41 +195,39 @@ scanCardView: {
     borderColor: '#ddd',
     borderBottomWidth: 0,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.8,
     shadowRadius: 2,
     elevation: 4,
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10,
-    backgroundColor: 'white'
-},
-buttonScan: {
+    backgroundColor: 'white',
+  },
+  buttonScan: {
     width: 42,
-
-},
-descText: {
+  },
+  descText: {
     padding: 16,
     textAlign: 'justify',
-    fontSize: 16
-},
+    fontSize: 16,
+  },
 
-
-highlight: {
+  highlight: {
     fontWeight: '700',
-},
+  },
 
-centerText: {
+  centerText: {
     flex: 1,
     fontSize: 18,
     padding: 32,
     color: '#777',
-},
-textBold: {
+  },
+  textBold: {
     fontWeight: '500',
     color: '#000',
-},
-buttonTouchable: {
+  },
+  buttonTouchable: {
     fontSize: 21,
     backgroundColor: '#ff0066',
     marginTop: 32,
@@ -215,10 +235,10 @@ buttonTouchable: {
     //width: deviceWidth - 62,
     justifyContent: 'center',
     alignItems: 'center',
-    height: 44
-},
-buttonTextStyle: {
+    height: 44,
+  },
+  buttonTextStyle: {
     color: 'white',
     fontWeight: 'bold',
-}
+  },
 });
